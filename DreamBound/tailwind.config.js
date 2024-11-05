6ce9d85053346c5a22e69c8e7704c392fdbb4406
Stephen-T-2023 <144911216+Stephen-T-2023@users.noreply.github.com>
2024-11-05 11:52:42 +0000

    removed animation

--- DreamBound/tailwind.config.js
+++ DreamBound/tailwind.config.js
@@ -20,15 +20,6 @@ export default {
       backgroundImage: {
         starrysky: "url('./assets/starrysky.jpg')"
       },
-      keyframes: {
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-3deg)" },
-          "50%": { transform: "rotate(3deg)" }
-        }
-      },
-      animation: {
-        wiggle: "wiggle 200ms ease-in-out"
-      }
     }
   },
   plugins: [],
